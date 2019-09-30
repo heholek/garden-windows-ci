42c3b24002774fd573e8d8ba6c71802699b9e715
Malini Valliath <mvalliath@pivotal.io>
2019-09-30 12:26:51 -0400

    Install later framework version

--- tasks/test-web-config-buildpack/run.ps1
+++ tasks/test-web-config-buildpack/run.ps1
@@ -244,7 +244,8 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 #$env:Path +=";%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install netfx-4.7.1-devpack -y
+#choco install netfx-4.7.1-devpack -y
+choco install netfx-4.7.2-devpack -y
 ./build.ps1 Test
 
 #main # Run the application.
