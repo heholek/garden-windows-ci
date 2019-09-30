90a67ffaa46a9fdf07663a8cfdecf3aa47fbe569
Malini Valliath <mvalliath@pivotal.io>
2019-09-30 11:50:25 -0400

    Mimic chocolatey install of bosh release

--- tasks/test-web-config-buildpack/run.ps1
+++ tasks/test-web-config-buildpack/run.ps1
@@ -237,8 +237,13 @@
 #    Write-Host "Hello Malini!"
 #}
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-Start-Sleep -Seconds 3
+#Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#Start-Sleep -Seconds 3
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+#$env:Path +=";%ALLUSERSPROFILE%\chocolatey\bin"
+
 choco install netfx-4.7.1-devpack -y
 ./build.ps1 Test
 
